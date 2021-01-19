f1f0757fec63fb9212b84f2da0af92610d7ed028
MalbonaGenio <MalbonaGenio@users.noreply.github.com>
2021-01-19 14:22:58 +0900

    added fetch api fucntion created a function getJokes that fetches from the jokes API and stores the info in the joke variable.

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-// Create 4 variables and assign them to their respective elements:
-//setupDiv, punchlinDiv, punchlineBtn, newJokeBtn
-
+//get the elemetnts we are going to use
 const setupDiv = document.getElementById("setup");
 const punchlineDiv = document.getElementById("punchline");
 const punchlineBtn = document.getElementById("punchlineBtn");
 const newJokeBtn = document.getElementById("newJokeBtn");
+
+//fecth data from the jokes API
+async function getJoke() {
+  const jokePromise = await fetch(
+    "https://official-joke-api.appspot.com/jokes/programming/random"
+  );
+  const joke = await jokePromise.json();
+
+  console.log(joke);
+}
+
+getJoke();
