21afe907cdeb731c3e7e4692a76b0a08a866369d
MalbonaGenio <MalbonaGenio@users.noreply.github.com>
2021-01-19 13:39:15 +0900

    added final styles and assigned variables to html elements

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+// Create 4 variables and assign them to their respective elements:
+//setupDiv, punchlinDiv, punchlineBtn, newJokeBtn
 
+const setupDiv = document.getElementById("setup");
+const punchlineDiv = document.getElementById("punchline");
+const punchlineBtn = document.getElementById("punchlineBtn");
+const newJokeBtn = document.getElementById("newJokeBtn");
