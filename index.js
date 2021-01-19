54f1128dd30581d8270dca7184f37f5b490931a2
MalbonaGenio <MalbonaGenio@users.noreply.github.com>
2021-01-20 08:30:49 +0900

    added the get punchline button Added a function that runs when get punch line is clicked. then we add class bubble to the punclineDiv and toogle the class of the two buttons to show the new joke button

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const setupDiv = document.getElementById("setup");
 const punchlineDiv = document.getElementById("punchline");
 const punchlineBtn = document.getElementById("punchlineBtn");
 const newJokeBtn = document.getElementById("newJokeBtn");
+let punchline;
+
+punchlineBtn.addEventListener("click", getPunchLine);
+
+function getPunchLine() {
+  punchlineDiv.innerHTML = punchline;
+  punchlineDiv.classList.add("bubble");
+  punchlineBtn.classList.toggle("hidden");
+  newJokeBtn.classList.toggle("hidden");
+}
 
 //fecth data from the jokes API
 async function getJoke() {
@@ -10,8 +20,12 @@ async function getJoke() {
     "https://official-joke-api.appspot.com/jokes/programming/random"
   );
   const joke = await jokePromise.json();
+  // Get the setup from the joke and insert it into the  element. We only have one object in the array so we access by just [0]
+  setupDiv.innerHTML = joke[0].setup;
+  punchline = joke[0].punchline;
 
-  console.log(joke);
+  punchlineBtn.classList.toggle("hidden");
+  newJokeBtn.classList.toggle("hidden");
 }
 
 getJoke();
